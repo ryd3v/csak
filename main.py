e43bbdf7612577951425de355b35165a88f2a9d8
ryd3v <hello@ryd3v.com>
2023-07-25 10:26:43 -0400

    add output to file

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import pexpect
 from tqdm import tqdm
 
 
-def scan_tcp_ports(ip, start_port, end_port=None):
+def scan_tcp_ports(ip, start_port, end_port=None, output_file=None):
     if end_port is None:
         end_port = 65535
 
@@ -30,10 +30,15 @@ def scan_tcp_ports(ip, start_port, end_port=None):
             port = futures[future]
             if future.result():
                 open_ports.append(port)
+            if output_file:
+                with open(output_file, 'w') as f:
+                    f.write("Open TCP ports on {}: {}\n".format(ip, open_ports))
+            else:
+                print("Open TCP ports on {}: {}".format(ip, open_ports))
     return open_ports
 
 
-def scan_udp_ports(ip, start_port, end_port=None):
+def scan_udp_ports(ip, start_port, end_port=None, output_file=None):
     if end_port is None:
         end_port = 65535
 
@@ -44,6 +49,11 @@ def scan_udp_ports(ip, start_port, end_port=None):
             port = futures[future]
             if future.result():
                 open_ports.append(port)
+            if output_file:
+                with open(output_file, 'w') as f:
+                    f.write("Open UDP ports on {}: {}\n".format(ip, open_ports))
+            else:
+                print("Open UDP ports on {}: {}".format(ip, open_ports))
     return open_ports
 
 
@@ -68,7 +78,7 @@ def scan_udp_port(ip, port):
 
 
 # Nikto
-def scan_with_nikto(url):
+def scan_with_nikto(url, output_file=None):
     try:
         # Run the Nikto command and capture the output
         command = f"nikto -host {url}"
@@ -76,12 +86,19 @@ def scan_with_nikto(url):
         for line in process.stdout:
             print(line, end='')
         process.wait()
+        if output_file:
+            with open(output_file, 'w') as f:
+                for line in process.stdout:
+                    f.write(line)
+        else:
+            for line in process.stdout:
+                print(line, end='')
     except subprocess.CalledProcessError as e:
         print(f"Error executing Nikto: {e.output}")
 
 
 # Netdiscover
-def run_netdiscover(ip_range):
+def run_netdiscover(ip_range, output_file=None):
     try:
         # Run the netdiscover command and capture the output
         command = f"sudo netdiscover -r {ip_range}"
@@ -97,13 +114,39 @@ def run_netdiscover(ip_range):
             print(line)
             if "Currently scanning: Finished!" in line:
                 break
+            if output_file:
+                with open(output_file, 'w') as f:
+                    while True:
+                        try:
+                            line = process.readline()
+                        except pexpect.exceptions.TIMEOUT:
+                            break
+                        if not line:
+                            break
+                        line = line.decode().strip()
+                        f.write(line + '\n')
+                        print(line)
+                        if "Currently scanning: Finished!" in line:
+                            break
+            else:
+                while True:
+                    try:
+                        line = process.readline()
+                    except pexpect.exceptions.TIMEOUT:
+                        break
+                    if not line:
+                        break
+                    line = line.decode().strip()
+                    print(line)
+                    if "Currently scanning: Finished!" in line:
+                        break
         process.wait()
     except subprocess.CalledProcessError as e:
         print(f"Error executing netdiscover: {e.output}")
 
 
 # Web Directory Scanning (using dirb)
-def run_dirb(url, wordlist):
+def run_dirb(url, wordlist, output_file=None):
     try:
         # Run the dirb command and capture the output
         command = f"dirb {url} {wordlist} -o dirb.txt -S"
@@ -111,6 +154,13 @@ def run_dirb(url, wordlist):
         for line in process.stdout:
             print(line, end='')
         process.wait()
+        if output_file:
+            with open(output_file, 'w') as f:
+                for line in process.stdout:
+                    f.write(line)
+        else:
+            for line in process.stdout:
+                print(line, end='')
     except subprocess.CalledProcessError as e:
         print(f"Error executing Dirb: {e.output}")
 
@@ -152,61 +202,59 @@ def main():
         print("3. Scan a URL with Nikto")
         print("4. Run netdiscover (must be root!)")
         print("5. Web Directory Scanning")
-        print("6. Exit")  # Add an option to exit the program
+        print("6. Exit")
         choice = int(input())
 
         if choice == 1:
             ip = input("Enter the IP address to scan: ")
             scan_option = input("Enter 'all' to scan all ports or 'range' to specify start and end ports: ")
+            output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
 
             if scan_option.lower() == 'all':
-                open_ports = scan_tcp_ports(ip, start_port=1, end_port=65535)
+                open_ports = scan_tcp_ports(ip, start_port=1, end_port=65535, output_file=output_file)
             elif scan_option.lower() == 'range':
                 start_port = int(input("Enter the starting port: "))
                 end_port = int(input("Enter the ending port: "))
-                open_ports = scan_tcp_ports(ip, start_port=start_port, end_port=end_port)
-            elif choice == 3:
-                url = input("Enter the URL you want to scan with Nikto: ")
-                scan_with_nikto(url)
+                open_ports = scan_tcp_ports(ip, start_port=start_port, end_port=end_port, output_file=output_file)
             else:
                 print("Invalid option. Please choose 'all' or 'range'.")
                 return
 
             print("Open TCP ports on {}: {}".format(ip, open_ports))
-            pass
 
         elif choice == 2:
             ip = input("Enter the IP address to scan: ")
             scan_option = input("Enter 'all' to scan all ports or 'range' to specify start and end ports: ")
+            output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
 
             if scan_option.lower() == 'all':
-                open_ports = scan_udp_ports(ip, start_port=1, end_port=65535)
+                open_ports = scan_udp_ports(ip, start_port=1, end_port=65535, output_file=output_file)
             elif scan_option.lower() == 'range':
                 start_port = int(input("Enter the starting port: "))
                 end_port = int(input("Enter the ending port: "))
-                open_ports = scan_udp_ports(ip, start_port=start_port, end_port=end_port)
+                open_ports = scan_udp_ports(ip, start_port=start_port, end_port=end_port, output_file=output_file)
             else:
                 print("Invalid option. Please choose 'all' or 'range'.")
                 return
 
             print("Open UDP ports on {}: {}".format(ip, open_ports))
-            pass
 
         elif choice == 3:
             url = input("Enter the URL you want to scan with Nikto: ")
-            scan_with_nikto(url)
-            pass
+            output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
+            scan_with_nikto(url, output_file=output_file)
 
         elif choice == 4:
-            ip_range = input("Enter the IP address range (e.g., 192.168.2.1/24). Press Ctrl+C to cancel the scan: ")
-            run_netdiscover(ip_range)
-            pass
+            ip_range = input("Enter the IP address range (e.g., 192.168.2.1/24). "
+                             "Press Ctrl+C to cancel the scan: ")
+            output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
+            run_netdiscover(ip_range, output_file=output_file)
 
         elif choice == 5:
             url = input("Enter the URL you want to scan with Dirb: ")
             wordlist = input("Enter the path to the wordlist file: ")
-            run_dirb(url, wordlist)
-            pass
+            output_file = input("Enter the output file path (or leave it blank to display results on the console): ")
+            run_dirb(url, wordlist, output_file=output_file)
 
         elif choice == 6:
             print("Exiting Cyber Swiss Army Knife Tool. Goodbye!")
